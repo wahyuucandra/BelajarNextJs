5d815592194160b9e010fa5bf28ed5b3da201c75
wahyu <wahyuucandra@gmail.com>
2023-06-21 13:13:32 +0700

    update

--- src/pages/api/country/create.tsx
+++ src/pages/api/country/create.tsx
@@ -10,22 +10,32 @@ export default async function handler(
 	res: NextApiResponse
 ) {
 
-	if(!req.method === 'POST'){ 
+	if(req.method !== 'POST'){ 
 		res.status(405).json({ error: 'Method Not Allowed' });
 	}
 
-	const { name, image_url } = req.body
+	const { name, image_url } = req.body 
     const country = await prisma.country.create({
         data:{
             name  : name,
 			slug : name.toLowerCase(),
-			image_url : image_url
+			image_url : image_url00
         }
-      })
+      }).catch(err =>{
+		res.status(300).json({
+			code:"F",
+			message: err.message,
+			data: []
+		});
+	  })
 	
 	await delay(2000);
 	if (country) {
-		res.status(200).json(country);
+		res.status(200).json({
+			code:"T",
+			message: "Success",
+			data: country
+		});
 	} else {
 		res.status(404).json({ error: 'Page Not found' });
 	}
--- src/pages/country/create.tsx
+++ src/pages/country/create.tsx
@@ -1,3 +1,5 @@
+//@ts-nocheck
+
 import { Inter } from 'next/font/google';
 import { Hero } from '@/sections/hero';
 import { Places } from '@/sections/places';
@@ -7,29 +9,55 @@ import Loading from '@/sections/loading';
 // import { COUNTRY_LIST } from '../../DATA/COUNTRY_LIST';
 
 const inter = Inter({ subsets: ['latin'] });
-
 export default function Create() {
+    var url  = "http://localhost:3000/api/country/create"
+    const [data, setData] = useState([])
+    const [formData, setFormData] = useState({
+        name: "",
+        image_url: "",
+    });
 
-    const [countryName, setCountryName] = useState("")
+    const submitForm  = async (e) => {
+        e.preventDefault()
 
-    const createCountry = async () =>{
-        console.log("clicked")
-        // const name = document.getElementById('name') as HTMLInputElement | null
-        // console.log(name ? name.value : "kosong ")
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+          }).then((res) => {
+            const response = res.json()
+            response.then((data) => {
+                alert(data.message)
+            })
+          })
+
+          return true
+    };
+
+    const handleInput = (e) => {
+        const fieldName = e.target.name;
+        const fieldValue = e.target.value;
+
+        setFormData((prevState) => ({
+            ...prevState,
+            [fieldName]: fieldValue
+        }));
     }
-
 
 	return (
         <main>
-            <form action="http://localhost:3000/api/country/create" method="POST" className="center-flex wd-30 gap-1">
+            <form id="createCountry" className="center-flex wd-30 gap-1" action="http://localhost:3000/list" onSubmit={submitForm}>
                 <div className="center-flex">
                     <label htmlFor="name">Name Country</label>
-                    <input type="text" name="name" required={true}/>
+                    <input type="text" name="name" onChange={handleInput} onClick={handleInput} required={true} />
                     
                     <label htmlFor="image_url">Link Image</label>
-                    <input type="text" name="image_url" required={true}/>
+                    <input type="text" name="image_url" onChange={handleInput} onClick={handleInput} required={true} />
                 </div>
-                <button>Create Country</button>
+                <button className="btn-submit">Create Country</button>
             </form>
         </main>
 	);
